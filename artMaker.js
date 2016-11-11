a3c95e0ad57e390d34a11f3da49b1ec0973b270d
Siyami Avci <siyamiavci@gmail.com>
2016-11-11 15:10:47 -0800

    blue brush works

--- artMaker.js
+++ artMaker.js
@@ -1,11 +1,34 @@
-const blueBrush = document.getElementById('blue');
-// const redBrush = document.getElementId('red');
-const paintRight = document.getElementById('paintRight');
+// const blueBrush = document.getElementById('blue');
+// const paintRight = document.getElementById('paintRight');
+// const paintLeft = document.getElementById('paintLeft');
+//
+// blueBrush.addEventListener('click', function() {
+//   paintRight.addEventListener('click', function() {
+//     // paintRight.style.backgroundColor = 'blue';
+//     document.getElementById('paintRight').style.backgroundColor =
+//       "blue";
+//   })
+// });
 
-blueBrush.addEventListener('click', function() {
-  paintRight.addEventListener('click', function() {
-    // paintRight.style.backgroundColor = 'blue';
-    document.getElementById('paintRight').style.backgroundColor =
-      "blue";
-  })
+////////////////////////////////////////////
+
+const blueBrush = document.getElementById('blue');
+const paintRight = document.getElementById('paintRight');
+const paintLeft = document.getElementById('paintLeft');
+
+blueBrush.addEventListener('click', function(event) {
+  const controls = document.querySelector('#container');
+
+  controls.addEventListener('click', (event) => {
+
+    if (event.target === paintRight) {
+      paintRight.classList.add('blue');
+    } else if (event.target === paintLeft) {
+      paintLeft.classList.add('blue');
+    }
+  });
+
 });
+
+
+////////////////
