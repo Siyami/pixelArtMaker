e9c99dc30088449aad9e9990bf0709caec6b4965
Siyami Avci <siyamiavci@gmail.com>
2016-11-11 16:05:04 -0800

    red brush works

--- artMaker.js
+++ artMaker.js
@@ -13,10 +13,16 @@
 ////////////////////////////////////////////
 
 const blueBrush = document.getElementById('blue');
+const redBrush = document.getElementById('red');
+
 const paintRight = document.getElementById('paintRight');
 const paintLeft = document.getElementById('paintLeft');
 
+
+
+// Function invokes if user clicks blueBrush
 blueBrush.addEventListener('click', function(event) {
+
   const controls = document.querySelector('#container');
 
   controls.addEventListener('click', (event) => {
@@ -30,5 +36,19 @@ blueBrush.addEventListener('click', function(event) {
 
 });
 
+// Function invokes if user clicks redBrush
+redBrush.addEventListener('click', function(event) {
 
-////////////////
+  const controls = document.querySelector('#container');
+  controls.addEventListener('click', (event) => {
+
+    if (event.target.id === 'paintRight') {
+      paintRight.classList.add('red');
+    } else if (event.target.id === 'paintLeft') {
+      paintLeft.classList.add('red');
+      console.log(paintLeft.classList);
+
+    }
+  });
+
+});
