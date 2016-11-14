f03a5ed8b6967a00441de11a7e72d55a03993f85
Siyami Avci <siyamiavci@gmail.com>
2016-11-14 00:01:03 -0800

    add CNAME

--- test.js
+++ test.js
@@ -10,3 +10,41 @@ paintColors.addEventListener('click', (event) => {
 paintBox.addEventListener('click', (event) => {
   event.target.setAttribute('class', "box " + color);
 });
+
+// or another solution
+
+// const blueBrush = document.getElementById('blue');
+// const redBrush = document.getElementById('red');
+//
+// const paintRight = document.getElementById('paintRight');
+// const paintLeft = document.getElementById('paintLeft');
+//
+// // Function invokes if user clicks blueBrush
+// blueBrush.addEventListener('click', function(event) {
+//
+//   const controls = document.querySelector('#container');
+//
+//   controls.addEventListener('click', (event) => {
+//
+//     if (event.target === paintRight) {
+//       paintRight.classList.add('blue');
+//     } else if (event.target === paintLeft) {
+//       paintLeft.classList.add('blue');
+//     }
+//   });
+// });
+//
+// // Function invokes if user clicks redBrush
+// redBrush.addEventListener('click', function(event) {
+//
+//   const controls = document.querySelector('#container');
+//   controls.addEventListener('click', (event) => {
+//
+//     if (event.target === paintRight) {
+//       paintRight.classList.add('red');
+//     } else if (event.target === paintLeft) {
+//       paintLeft.classList.add('red');
+//       console.log(paintLeft.classList);
+//     }
+//   });
+// });
